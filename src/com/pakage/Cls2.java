d38a88fcdc5273af9f1e99397030eb2ac0cd6231
jatinfaridkot <jk116845@gmail.com>
2023-09-24 15:30:22 +0530

    add

---
+++ src/com/pakage/Cls2.java
@@ -0,0 +1,8 @@
+package com.pakage;
+
+public class Cls2 {
+	public static void main(String[] args) {
+
+		System.out.println("No");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author CITS
+ *
+ */
+module Student {
+}
