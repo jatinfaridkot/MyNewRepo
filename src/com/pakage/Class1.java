a4cc9ebb89bbeb1d58053c24920d3094579489b6
Jatin <143738215+jatinfaridkot@users.noreply.github.com>
2023-09-24 18:37:25 +0530

    Update Class1.java

--- src/com/pakage/Class1.java
+++ src/com/pakage/Class1.java
@@ -2,7 +2,7 @@ package com.pakage;
 
 public class Class1 {
 	public static void main(String[] args) {
-		System.out.println("all Student very Good boy");
-		System.out.println("not  Good to ");
+		System.out.println("Ram");
+		
 	}
 }
